7d288190db0acefc24d67b456f73995752a30792
Stepani2YT <stepapugavka@gmail.com>
2025-01-15 21:23:14 +0300

    Add files via upload

---
+++ defalt.py
@@ -0,0 +1,54 @@
+from os import system
+from time import sleep
+
+#update_programs
+system('apt update -y')
+#git
+system('clear')
+print('#######')
+print('# GIT #')
+print('#######')
+sleep(1)
+system('apt install git -y')
+#filezila
+system('clear')
+print('###########')
+print('#filezila#')
+print('###########')
+sleep(1)
+system('apt install filezila -y')
+#zenmap
+system('clear')
+print('########')
+print('#zenmap#')
+print('########')
+sleep(1)
+system('apt install zenmap -y')
+#metasploit-framework
+system('clear')
+print('######################')
+print('#metasploit-framework#')
+print('######################')
+sleep(1)
+system('apt install metasploit-framework -y')
+#w3af
+system('clear')
+print('######')
+print('#w3af#')
+print('######')
+sleep(1)
+system('apt-get install w3af')
+#passgen
+system('clear')
+print('#########')
+print('#PassGen#')
+print('#########')
+sleep(1)
+system('git clone https://github.com/UnderMind0x41/PassGen.git')
+#Wifite2
+system('clear')
+print('###########')
+print('#Wifite_v2#')
+print('###########')
+sleep(1)
+system('git clone https://github.com/derv82/wifite2.git')
